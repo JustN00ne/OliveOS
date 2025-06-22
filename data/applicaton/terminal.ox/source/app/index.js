e486290ee1fb32b077a8e46c6d7ea118ffccbd8b
JustN00ne <justnoone963@gmail.com>
2025-06-22 19:05:29 +0200

    idk

--- data/applicaton/terminal.ox/source/app/index.js
+++ data/applicaton/terminal.ox/source/app/index.js
@@ -1,1 +1,7 @@
 // This file is intentionally left blank for the terminal app.
+
+// Remove device info simulation, restore olive command to simple info
+// In runCommand (inside terminal.ox app), replace 'olive' command logic:
+// case 'olive':
+//   term.writeln('OliveOS Terminal - Simulated shell.\nCopyright (c) 2023-2025 OliveOS.');
+//   break;
